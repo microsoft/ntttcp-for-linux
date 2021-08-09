5d4abec070697092fc6f3944e0c85e9dbc60ea24
sunnyguoqq <sunnyguoqq@outlook.com>
2021-08-09 00:55:39 +0000

    Modify the function name of the output xml file.

--- src/util.c
+++ src/util.c
@@ -264,7 +264,7 @@ void print_test_results(struct ntttcp_test_endpoint *tep)
 	printf("---------------------------------------------------------\n");
 
 	if (tep->test->save_xml_log)
-		if (write_result_into_log_file(tep) != 0)
+		if (write_result_into_xml_file(tep) != 0)
 			PRINT_ERR("Error writing log to xml file");
 	if (tep->test->save_json_log)
                 if (write_result_into_json_file(tep) != 0)
@@ -345,7 +345,7 @@ unsigned int escape_char_for_json(char *in, char *out) {
 	return count;
 }
 
-int write_result_into_log_file(struct ntttcp_test_endpoint *tep)
+int write_result_into_xml_file(struct ntttcp_test_endpoint *tep)
 {
 	struct ntttcp_test *test = tep->test;
 	struct ntttcp_test_endpoint_results *tepr = tep->results;
